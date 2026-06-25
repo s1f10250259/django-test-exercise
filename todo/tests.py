2b6cf9434c83aa694deba8951e29495044484ccf
1F10250259 <s1f102502597@iniad.org>
2026-06-25 11:56:41 +0900

    警告修正5

--- todo/tests.py
+++ todo/tests.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from todo.models import Task
 
 
-#Create your tests here.
+#　Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
@@ -52,7 +52,7 @@ class TaskModelTestcase(TestCase):
         task.save()
 
         self.assertFalse(task.is_overdue(current))
-    
+
 
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
@@ -65,7 +65,7 @@ class TodoViewTestCase(TestCase):
 
     def test_index_post(self):
         client = Client()
-        data = {'title' : 'Test Task', 'due_at' : '2024-06-30 23:59:59'}
+        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
         response = client.post('/', data)
 
         self.assertEqual(response.status_code, 200)
@@ -86,7 +86,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context['tasks'][1], task1)
 
     def test_index_get_order_due(self):
-        task1 = Task(title= 'task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
         task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
@@ -97,4 +97,3 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
-        
--- todo/views.py
+++ todo/views.py
@@ -8,7 +8,7 @@ from todo.models import Task
 def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
-        due_at=make_aware(parse_datetime(request.POST['due_at'])))
+                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
     if request.GET.get('order') == 'due':
         tasks = Task.objects.order_by('due_at')
